603e37246cc4c85dda3705d205d2d0df14e03265
Chandima Ranaweera <chan4lk@gmail.com>
2018-03-09 04:53:44 +0530

    completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,17 +4,29 @@ class ToDo extends React.Component {
     constructor() {
         super();
         this.state = {            
-            todos: ['Add Todo', 'Search Todo', 'Delete Todo']
+            todos: [
+                {text:'Add Todo', completed: false},
+                {text:'Delete Todo', completed: false},
+                {text:'Complete Todo', completed: false}
+               
+            ]
         };
         this.onAdd = this.onAdd.bind(this);
+        this.onCompleted = this.onCompleted.bind(this);
     }
 
     onAdd(e) {
         e.preventDefault();
         if (this.input.value) {
-            this.setState({ todos: [...this.state.todos, this.input.value] });
+            this.setState({ todos: [...this.state.todos, {text: this.input.value, completed: false}] });
             this.input.value = '';
         }
+    }
+
+    onCompleted(index){
+        let todos = this.state.todos;
+        todos[index].completed = !todos[index].completed;       
+        this.setState({todos: [...todos]});
     }
 
     render() {
@@ -26,7 +38,14 @@ class ToDo extends React.Component {
                 </form>
                 <div className="todos">
                     {this.state.todos.map((todo, index) => {
-                        return <div key={index}>{todo}</div>
+                        return (
+                        <div key={index} style={{textDecoration:todo.completed? 'line-through' : 'none'}}>
+                            <input 
+                            type="checkbox" 
+                            checked={todo.completed} 
+                            onClick={()=>this.onCompleted(index)}/>
+                            {todo.text}
+                        </div>)
                     })}
                 </div>
             </div>);
